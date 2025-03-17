ed9b8878f49518f22f072f2b1e2aaf55226c8123
Zair Bulos <buloszair@gmail.com>
2025-03-17 11:45:56 -0300

    Add Singleton

---
+++ creational-patterns/singleton/src/main/java/guru/refactoring/Main.java
@@ -0,0 +1,13 @@
+package guru.refactoring;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance("FOO");
+        Singleton s2 = Singleton.getInstance("BAR");
+
+        System.out.println(s1.getValue());
+        System.out.println(s2.getValue());
+    }
+
+}
