89fefa01211b7bfa5735ea186e357206d8cee5f2
Zair Bulos <buloszair@gmail.com>
2025-03-27 10:59:22 -0300

    Add composite

---
+++ structural-patterns/composite/src/main/java/guru/refactoring/shapes/Shape.java
@@ -0,0 +1,19 @@
+package guru.refactoring.shapes;
+
+import java.awt.*;
+
+/**
+ * Component
+ */
+public interface Shape {
+    int getX();
+    int getY();
+    int getWidth();
+    int getHeight();
+    void move(int x, int y);
+    boolean isInsideBounds(int x, int y);
+    void select();
+    void unSelect();
+    boolean isSelected();
+    void paint(Graphics graphics);
+}
